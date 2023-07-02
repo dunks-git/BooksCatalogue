f4fcd2454b8389e93c8c2fce9ae127916f112650
Linards Jubels <linardsjubels@gmail.com>
2023-07-02 17:25:04 +0300

    vue hello worked

--- resources/views/vue/base.blade.php
+++ resources/views/vue/base.blade.php
@@ -6,23 +6,24 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vue</title>
-    <script  src="{{ asset('js/vue3') }}"></script>
+    <script src="{{ asset('js/vue3.js') }}"></script>
 
 </head>
 <body>
 <div id="app">
-    <example-component>greeting</example-component>
+    ${ message }
 </div>
-<script>
 
-    Vue.createApp({
-        delimiters: ["${", "}"],
-        data(){
-            return{
-                greeting:'Hello World!'
-            };
-        }
-    }).mount('#app');
+<script type="module">
+    var vuapp = Vue.createApp({
+        data() {
+            return {
+                message: 'Hello Vue!',
+            }
+        },
+    })
+    vuapp.config.compilerOptions.delimiters = ['${', '}']
+    vuapp.mount('#app')
 </script>
 
 </body>
