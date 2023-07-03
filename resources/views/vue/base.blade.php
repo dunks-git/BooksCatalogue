4927b536b8ede3e2d1e78b7f56c48c43472d0cce
Linards Jubels <linardsjubels@gmail.com>
2023-07-03 08:22:06 +0300

    vue2

--- resources/views/vue/base.blade.php
+++ resources/views/vue/base.blade.php
@@ -7,23 +7,65 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vue</title>
     <script src="{{ asset('js/vue3.js') }}"></script>
+    <style>
+        html, body {
+            height: 100%;
+        }
+
+        body {
+            display: grid;
+            place-items: center;
+        }
+
+        .text-red {
+            color: #F00;
+        }
+
+        .text-green {
+            color: #0F0;
+        }
+    </style>
 
 </head>
 <body>
 <div id="app">
-    ${ message }
+        <div><input type="text" v-model="greeting"></div>
+        <p>${ greeting } (${ greeting.length })</p>
+    {{--    <button v-bind:class="buttonClasses">Click me</button>--}}
+{{--    <button :class="buttonClasses" v-on:click="toggle">Click me</button>--}}
+{{--    <button :class="active ? 'text-red' : 'text-green'" v-on:click="toggle">Click me</button>--}}
+    <button :class="active ? 'text-red' : 'text-green'" @click="toggle">Click me</button>
 </div>
 
 <script type="module">
     var vuapp = Vue.createApp({
         data() {
             return {
-                message: 'Hello Vue!',
+                greeting: 'Hello Vue!',
+                buttonClasses: 'text-green',
+                active: true
             }
         },
+
+        mounted() {
+            setTimeout(() => {
+                this.greeting = 'Changed'
+            }, 3000)
+        },
+
+        methods: {
+            toggle() {
+                // this.buttonClasses = 'text-red'
+                this.active = !this.active
+            }
+
+        },
+
+
     })
     vuapp.config.compilerOptions.delimiters = ['${', '}']
     vuapp.mount('#app')
+
 </script>
 
 </body>
