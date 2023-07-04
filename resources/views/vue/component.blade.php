b47eeafd3b4d8aff764913d069f40a68b67c1c47
Linards Jubels <linardsjubels@gmail.com>
2023-07-04 14:07:41 +0300

    components1

--- resources/views/vue/component.blade.php
+++ resources/views/vue/component.blade.php
@@ -5,48 +5,35 @@
 @endsection
 @section('content')
     <div id="app">
-        <section v-show="inProgressAssignments.length">
-            <h2 class="font-bold mb-2">In Progress</h2>
-            <ul>
-                <li v-for="assignment in assignments.filter(a=>!a.complete)" v-bind:key="assignment.id">
-                    <label>${assignment.name} <input type="checkbox" v-model="assignment.complete"></label>
-                </li>
-            </ul>
-            {{--            <pre>--}}
-            {{--                ${ assignments }--}}
-            {{--            </pre>--}}
-        </section>
-        {{--        <section v-show="assignments.filter(a=>a.complete).length" class="mt-3">--}}
-        <section v-if="completedAssignments.length" class="mt-3">
-            <h2 class="font-bold mb-2">Completed</h2>
-            <ul>
-                <li v-for="assignment in assignments.filter(a=>a.complete)">
-                    <label>${assignment.name} <input type="checkbox" v-model="assignment.complete"
-                                                     v-bind:key="assignment.id"></label>
-                </li>
-            </ul>
-        </section>
+        <app-button>Submit</app-button>
     </div>
     <script>
+
         let app = {
-            data() {
-                return {
-                    assignments: [
-                        {name: 'Finish project', complete: false, id: 1},
-                        {name: 'Read chapter 4', complete: false, id: 2},
-                        {name: 'Turn in homework', complete: false, id: 3}
-                    ]
-                }
-            },
-            computed: {
-                inProgressAssignments() {
-                    return this.assignments.filter(a => !a.complete)
+            components: {
+                'app-button': {
+                    template: `
+<button  class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing" ><slot /></button>
+                    `,
+                    data() {
+                        return {
+                            processing: true
+                        };
+                    },
+                    mounted() {
+                        // alert('app-button mounted');
+                    }
                 },
-                completedAssignments() {
-                    return this.assignments.filter(a => a.complete)
-                }
+
+                'assignments-list': {},
+                'assignments-list-item': {},
+                'accordion': {}
             }
         };
+        let AppButton = {};
+        let AsssignmentList = {};
+        let AsssignmentListItem = {};
+
         var vuapp = Vue.createApp(app);
         vuapp.config.compilerOptions.delimiters = ['${', '}'];
         vuapp.mount('#app');
