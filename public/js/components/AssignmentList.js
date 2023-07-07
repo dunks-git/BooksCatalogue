1ed69b9a1f669e3f211b52ef025d2ed14b45afae
Linards Jubels <linardsjubels@gmail.com>
2023-07-07 13:36:34 +0300

    component responsibility

--- public/js/components/AssignmentList.js
+++ public/js/components/AssignmentList.js
@@ -1,51 +1,45 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
-    components: {Assignment},
+    components: {AssignmentTags, Assignment},
     template: `
         <section v-show="assignments.length">
         <h2 className="font-bold mb-2">@{{ title }} <span>(@{{ assignments.length }})</span></h2>
 
-        <div class="flex gap-2">
-            <button
-                @click="currentTag = tag"
-                v-for="tag in tags"
-                class="border rounded px-1 py-px text-xs"
-                v-bind:class="{'border-blue-500 text-blue-500': tag === currentTag}"
-            >@{{ tag }}
-            </button>
-        </div>
+        <assignment-tags
+            :initial-tags="assignments.map(a => a.tag)"
+            :current-tag="currentTag"
+            @change="currentTag=$event"
+        />
 
         <ul class="border border-gray-600 p-3 divide-y mt-4">
             <Assignment v-for="assignment in filteredAssignments" v-bind:key="assignment.id" :assignment="assignment"/>
         </ul>
         </section>
     `,
-    props: {
-        assignments: Array,
-        title: String
-
-    },
-    computed: {
-
-        filteredAssignments() {
-            // return this.assignments;
-            if (this.currentTag === 'all') {
-                return this.assignments;
-            }
-
-            return this.assignments.filter(a => a.tag === this.currentTag);
-        },
-        tags() {
-            // return ['science', 'math', 'reading'];
-            // return this.assignments.map(a=>a.tag);
-            return new Set(['all', ...new Set(this.assignments.map(a => a.tag))]);
-        }
-
-    },
     data() {
         return {
             currentTag: 'all'
         };
+    },
+    props: {
+        assignments: Array,
+        title: String
+    },
+    computed: {
+        filteredAssignments() {
+            if (this.currentTag === 'all') {
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        }
+
+    },
+    methods:{
+        updateCurrentTag(e){
+            this.currentTag = e;
+        }
+
     }
 }
